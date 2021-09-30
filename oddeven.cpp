2c9991b1971b788074af4a82b27f941c92f8f1b0
Debosmita Bhattacharjee <78953484+theLIT02@users.noreply.github.com>
2021-09-30 19:58:01 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter the number: ";
+    cin>> n;
+
+    if (n%2==0){
+        cout<<"The number is Even"<<endl;
+    }
+    else {
+        cout<<"The number is odd"<<endl;
+    }
+    return 0;
+} 
